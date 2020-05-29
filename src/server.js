66b969e9a577940a329499104d9a3d48178a8ced
niv <NivKeidan@gmail.com>
2020-05-30 00:41:30 +0300

    preparing for production

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 var cors = require('cors');
+var compression = require('compression');
+var helmet = require('helmet');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const hostname = '127.0.0.1';
+const hostname = 'https://dry-falls-97387.herokuapp.com/';
 const data_file="entries.json";
-const port = 3333;
+const port = 80;
 
 const corsOptions = {
     origin: 'http://localhost:5000',
@@ -12,6 +14,8 @@ const corsOptions = {
 };
 
 const app = express();
+app.use(helmet());
+app.use(compression()); //Compress all routes
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
@@ -19,6 +23,7 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => {
     fs.readFile(data_file, 'utf8', (err, data) => {
         if (err) {
+
             res.status(500).send("error reading data file");
             return;
         }
@@ -29,7 +34,7 @@ app.get('/', (req, res) => {
 app.options('/', cors());
 
 app.post('/', function (req, res) {
-    fs.writeFileSync(data_file, JSON.stringify(req.body), e => {
+    fs.writeFileSync(data_file, JSON.stringify({timestamp: Date.now(), data: req.body}), e => {
         res.sendStatus(500);
     });
     res.sendStatus(200);
